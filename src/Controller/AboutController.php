f8156f8962e124586b284ed2c09ee5f440f3f67f
Lenar <linz14@mail.ru>
2021-07-01 09:24:31 +0300

    About page styling

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+  /**
+   * @Route("/about")
+   */
+  public function about(): Response
+  {
+    return $this->render('about.html.twig', []);
+  }
+}
