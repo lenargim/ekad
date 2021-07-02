9218befcf8b3de05a3b7cdc89467144b0b73c4b1
Lenar <linz14@mail.ru>
2021-07-02 16:13:32 +0300

    Submenu

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeamController extends AbstractController
 {
   /**
-   * @Route("/team")
+   * @Route("about/team")
    */
   public function team(): Response
   {
